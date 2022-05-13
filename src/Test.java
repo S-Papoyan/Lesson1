830e7d397c375d90057d22f561c5307d3e81d3db
unknown <you@example.com>
2022-05-13 10:38:43 +0400

    commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int x = 5, y = 4, z = 11;
+        y = x++;
+
+        z = y-- - ++x;
+
+        x = ++z + --y;
+
+        y = z++ + ++y + --x;
+
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        System.out.println("Z = " + z);
+    }
+}
+
+// x = 1, y = 4, z = 0
