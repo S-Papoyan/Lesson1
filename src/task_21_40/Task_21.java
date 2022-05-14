08d07d0886e35181d07c054f37f7c9ab8f8ae69b
Sergey <spapoyan1987@gmail.com>
2022-05-14 15:50:10 +0400

    commit

---
+++ src/task_21_40/Task_21.java
@@ -0,0 +1,16 @@
+package task_21_40;
+
+// Տրված են a,b,c թվերը, կազմել ծրագիր, որը կարտածի տրված թվերից մեծի արժեքը։
+
+public class Task_21 {
+    public static void main(String[] args) {
+
+        int a = 15, b = 12, c = 11;
+
+        if (a > b && a > c) {
+            System.out.println(a);
+        } else if (b > a && b > c) {
+            System.out.println(b);
+        } else System.out.println(c);
+    }
+}
