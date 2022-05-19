1d11860faa0ec239408b0cee8d5d1c7ab78ffeac
S-Papoyan <spapoyan1987@gmail.com>
2022-05-19 11:12:20 -0700

    commit

--- Lesson2.java
+++ Lesson2.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Lesson2 {
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 11, 4, 5, 11};
+
+        int x = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            x = x ^ arr[i];
+
+        }
+        System.out.println(x);
+    }
 }
+
